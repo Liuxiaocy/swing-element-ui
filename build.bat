2de049d0152b5bd27f057218f4f30c0083106892
dev <dev@local>
2026-08-20 00:08:33 +0800

    build: prefer local JDK 8 when present

--- build.bat
+++ build.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
-where javac >nul 2>nul || (echo ERROR: javac not on PATH & exit /b 1)
+set "JAVAC=javac"
+if exist "C:\Program Files\Java\jdk1.8.0_311\bin\javac.exe" set "JAVAC=C:\Program Files\Java\jdk1.8.0_311\bin\javac.exe"
+"%JAVAC%" -version >nul 2>nul || (echo ERROR: javac not found & exit /b 1)
 if not exist out mkdir out
 (for /f "delims=" %%f in ('dir /s /b src\*.java') do set "p=%%f" & echo "!p:\=/!") > .sources.txt
-javac -encoding UTF-8 --release 8 -d out @.sources.txt
+"%JAVAC%" -encoding UTF-8 --release 8 -d out @.sources.txt
 if errorlevel 1 (
   echo --release 8 not supported, retrying with -source/-target 8
-  javac -encoding UTF-8 -source 8 -target 8 -d out @.sources.txt
+  "%JAVAC%" -encoding UTF-8 -source 8 -target 8 -d out @.sources.txt
 )
 if errorlevel 1 (echo BUILD FAILED & exit /b 1)
 echo BUILD OK
